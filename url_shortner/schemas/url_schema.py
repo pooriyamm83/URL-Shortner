de74ffd7bd3064aebf2c39fc9e5aac3dd4609eb0
Morteza <maddahmasoud@gmail.com>
2025-12-12 17:11:40 +0330

    feat : Add URL models using Pydantic"

---
+++ url_shortner/schemas/url_schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, HttpUrl
+from datetime import datetime
+
+class URLCreate(BaseModel):
+    original_url: HttpUrl
+
+class URLResponse(BaseModel):
+    id: int
+    original_url: str
+    short_code: str
+    created_at: datetime
+
+class URLListResponse(BaseModel):
+    data: list[URLResponse]
+
+class APIResponse(BaseModel):
+    status: str
+    message: str | None = None
+    data: dict | list | None = None
